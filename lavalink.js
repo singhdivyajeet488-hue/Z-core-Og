1d37eb92a4dfa87595fdc2ee4a4f81b7d17e587b
singhdivyajeet488-hue <262640915+singhdivyajeet488-hue@users.noreply.github.com>
2026-06-15 05:12:09 +0530

    Add files via upload

---
+++ lavalink.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+    enabled: true, 
+    lavalink: {
+      name: "GlaceYT",
+      password: "glace",
+      host: "de-01.strixnodes.com",
+      port: 2010,
+      secure: false
+    }
+};           
+
+
+
+
